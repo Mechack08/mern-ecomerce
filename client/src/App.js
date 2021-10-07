79c1cbf4c377bf3c06cf0e9d50c69b8a265d2e4a
8pro <mechackkobius@gmail.com>
2021-10-07 09:54:50 +0300

    init project

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import ProductList from "./pages/ProductList";
+import Home from "./pages/Home";
+import "./style.css";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
